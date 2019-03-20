04274c698af6409e9df6efddf18cddd5a5f097a4
ybh <baodecaiying@qq.com>
2019-03-21 05:04:57 +0800

    提交
    Signed-off-by: ybh <baodecaiying@qq.com>

--- SifSysUserQryDao.java
+++ SifSysUserQryDao.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Sort;
 
 import cn.sunline.domain.endauth.SifSysUserQry;
 import cn.sunline.domain.endauth.SifSysUserQryPk;
-22222
+22222333
 public interface SifSysUserQryDao {
 
 	public Page<SifSysUserQry> findAll(Pageable pageable);
