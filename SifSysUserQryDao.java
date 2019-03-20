47c4ab837af89ae3975de7f21b0752213ac5ed41
ybh <baodecaiying@qq.com>
2019-03-21 05:10:56 +0800

    提交

--- SifSysUserQryDao.java
+++ SifSysUserQryDao.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Sort;
 
 import cn.sunline.domain.endauth.SifSysUserQry;
 import cn.sunline.domain.endauth.SifSysUserQryPk;
-kkkkkkk111111111333222
+kkkkkk
 public interface SifSysUserQryDao {
 
 	public Page<SifSysUserQry> findAll(Pageable pageable);
